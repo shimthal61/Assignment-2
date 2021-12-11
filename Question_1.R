116b2cb0f5db5b94a94753d193994543aa6f7f7c
Harvey Schneider <harvey.schneider@student.manchester.ac.uk>
2021-12-11 19:32:29 +0000

    added seed to visualisation

--- Question_1.R
+++ Question_1.R
@@ -18,10 +18,12 @@ q1_data %>%
   summarise(mean = mean(response_time), sd = sd(response_time))
 
 #Data Visualisation
+# Use the set.seed function when running simulations to ensure all results, figures, etc are reproducible.
+set.seed(42)
 q1_data %>% 
   ggplot(aes(x = visual_quality, y = response_time, colour = visual_quality)) +
   geom_violin(width = 0.3) +
-  geom_jitter(width = 0.1, alpha = 0.5) +
+  geom_point(position = position_jitter(width = 0.15, seed = 42)) +
   theme_minimal() +
   stat_summary(fun.data = "mean_cl_boot", colour = "black") +
   guides(colour = 'none') +
@@ -29,5 +31,7 @@ q1_data %>%
        x = "Visual Quality",
        y = "Reaction Time (ms)") +
   theme(text = element_text(size = 13))
-  
-        
+
+
+model1 <- aov_4(response_time ~ visual_quality + (1 | participant), data = q1_data)  
+
