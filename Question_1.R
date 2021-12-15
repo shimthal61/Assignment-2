fa2fe4cd13a58c73f8c3f054d14975671aea7db4
Harvey Schneider <50023408+shimthal61@users.noreply.github.com>
2021-12-15 19:18:24 +0000

    Built our ANOVA model

--- Question_1.R
+++ Question_1.R
@@ -5,27 +5,24 @@ raw_data_1 <- read_csv("assignment_2_dataset_1.csv")
 head(raw_data_1)
 
 #Tidying our data
-q1_data <- raw_data_1 %>%
+q1_data_tidied <- raw_data_1 %>%
   rename(visual_quality = condition) %>% 
   mutate(visual_quality = recode(visual_quality,
                             "condition_a" = "Normal",
                             "condition_b" = "Degraded")) %>% 
   mutate(visual_quality = factor(visual_quality))
 
-head(q1_data)
+head(q1_data_tidied)
 
 #Doesn't look like there is any missing data
-vis_miss(q1_data)
+vis_miss(q1_data_tidied)
 
 #Summarising our data
-q1_data %>% 
+q1_data_tidied %>% 
   group_by(visual_quality) %>% 
   summarise(mean = mean(response_time), sd = sd(response_time)) %>% 
   arrange(mean)
 
-q1_data %>% 
-  group_by(visual_quality) %>% 
-  arrange(response_time)
 
 #It appears at a glance that participants in the normal condition has a faster response time.
 
@@ -33,7 +30,10 @@ q1_data %>%
 # Use the set.seed function when running simulations to ensure all results, figures, etc are reproducible.
 set.seed(42)
 
-q1_data %>% 
+
+showtext_auto()
+
+q1_data_tidied %>% 
   mutate(visual_quality = fct_relevel(visual_quality, "Normal", "Degraded")) %>% 
   ggplot(aes(x = visual_quality, y = response_time, colour = visual_quality)) +
   geom_violin(width = 0.3) +
@@ -46,26 +46,10 @@ q1_data %>%
   labs(title = "Examining the effect of visual quality on response times",
        x = "Visual Quality",
        y = "Response Time (ms)") +
-  theme(text = element_text(size = 13))
-
-q1_data %>% 
-  mutate(visual_quality = fct_relevel(visual_quality, "Normal", "Degraded")) %>% 
-  ggplot(aes(x = visual_quality, y = response_time, colour = visual_quality)) +
-  geom_violin(width = 0.3) +
-  geom_point(alpha = 0.8, position = position_jitter(width = 0.15, seed = 42)) +
-  theme_minimal() +
-  stat_summary(fun.data = "mean_cl_boot", colour = "black") +
-  guides(colour = 'none') +
-  scale_y_continuous(breaks = seq(925, 1075, by = 25),
-                     limits = c(925, 1075)) +
-  labs(title = "Examining the effect of visual quality on response times",
-       x = "Visual Quality",
-       y = "Response Time (ms)") +
-  theme(text = element_text(family = "Lato"))
-
+  theme(text = element_text(family = "Lato", size = 13))
 
 #F value is pretty large and p < 0.001. However, we don't know  what's driving the difference yet
-model1 <- aov_4(response_time ~ visual_quality + (1 | participant), data = q1_data)
+between_anova <- aov_4(response_time ~ visual_quality + (1 | participant), data = q1_data_tidied)
 
 anova(model1)
 
